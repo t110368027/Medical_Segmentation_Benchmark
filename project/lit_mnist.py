027c2579a617cfbf6709e56587e214f7e980d9b3
ryan <t110368927@ntut.edu.tw>
2023-01-25 23:41:20 +0800

    update test ci

--- project/lit_mnist.py
+++ project/lit_mnist.py
@@ -91,6 +91,21 @@ def cli_main():
     # ------------
     trainer.test(test_dataloaders=test_loader)
 
+def mnist():
+    pl.seed_everything(1234)
+    parser = ArgumentParser()
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = LitClassifier.add_model_specific_args(parser)
+    args = parser.parse_args()
+
+    dataset = MNIST('', train=True, download=True, transform=transforms.ToTensor())
+    mnist_test = MNIST('', train=False, download=True, transform=transforms.ToTensor())
+    mnist_train, mnist_val = random_split(dataset, [55000, 5000])
+    train_loader = DataLoader(mnist_train, batch_size=args.batch_size)
+    val_loader = DataLoader(mnist_val, batch_size=args.batch_size)
+    test_loader = DataLoader(mnist_test, batch_size=args.batch_size)
+    return train_loader, val_loader, test_loader
 
 if __name__ == '__main__':
     cli_main()
--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,6 +1,6 @@
 from pytorch_lightning import Trainer, seed_everything
 from project.lit_mnist import LitClassifier
-from project.datasets.mnist import mnist
+from project.lit_mnist import mnist
 
 
 def test_lit_classifier():
